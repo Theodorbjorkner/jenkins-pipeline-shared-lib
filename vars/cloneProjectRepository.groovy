a20001572578b2fe5a9df34c1806dcf505c973f9
Theodorbjorkner <theodor.bjorkner@gmail.com>
2024-11-03 10:13:17 +0100

    created clone project repository method Clone project repository

---
+++ vars/cloneProjectRepository.groovy
@@ -0,0 +1,21 @@
+import cicd.config.DeploymentConfigs
+
+public void call(String repositoryName, String sourceBranch){
+    // fail if source brand is not provided
+    if(!sourceBranch){
+        error ("source branch for git clone is '${sourceBranch}'");
+    }
+    // Clone repo
+    final String gitUrl = _generateGitUrl(repositoryName);
+    checkout (
+            scm:
+            scmGit(branches: [[name: "refs/heads/${sourceBranch}"]],
+                    extensions: [],
+                    userRemoteConfigs: [[url: gitUrl]]),
+            changelog: true, poll: false);
+}
+ private String _generateGitUrl(String repositoryName){
+    final String remoteUrlPattern = DeploymentConfigs.globalConfigs["git"]["remoteHTTPS"];
+    return remoteUrlPattern.replace("<REPOSITORY_NAME>", repositoryName);
+
+}
