1109ceb92249629ed8baab2f433dc4401d6d1f46
Theodorbjorkner <theodor.bjorkner@gmail.com>
2024-11-03 10:44:46 +0100

    created readCicdProjectConfig.groovy read project config

---
+++ vars/readCicdProjectConfig.groovy
@@ -0,0 +1,12 @@
+import cicd.config.DeploymentConfigs;
+
+public Map call (){
+    final Map cicdConfigMap = DeploymentConfigs.globalConfigs["cicdConfig"];
+    final String pipelineConfigFolderPath = cicdConfigMap["pipelineConfigFolderPath"];
+    final GString pipelineConfigYamlPath= "${pipelineConfigFolderPath}/pipeline-config.yaml";
+
+    // read config
+    final Map pipelineConfigMap = readYaml(file: globalConfigsResources);
+    return pipelineConfigMap;
+
+}
