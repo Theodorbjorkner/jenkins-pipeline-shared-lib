f990f5bdacbf80847503883e0d691e8297f1e02b
Theodorbjorkner <theodor.bjorkner@gmail.com>
2024-11-04 22:54:24 +0100

    Created installDependencies()scrips install dependencies

--- vars/installDependencies.groovy
+++ vars/installDependencies.groovy
@@ -2,7 +2,7 @@ public void call(){
     _npmInstall();
 }
 
-private void _npmInstall(){
+private void _npmInstall() {
     // if nmp ci doesnt work, use npm install
     sh(script: "npm ci", label: "Install npm dependencies");
 }
