f48fe3806bbed6555e950c03b8c93cae1cba6f21
Theodorbjorkner <theodor.bjorkner@gmail.com>
2024-11-04 13:04:13 +0100

    created readCicdProjectConfig.groovy read project config

--- vars/cloneProjectRepository.groovy
+++ vars/cloneProjectRepository.groovy
@@ -7,23 +7,23 @@ public void call(String repositoryName, String sourceBranch){
     }
     // Clone repo
     final String gitUrl = _generateGitUrl(repositoryName);
-    //checkout(
-      //      scm: [
-        //      $class:"GitSCM",
-          //          userRemoteConfigs: [[url:gitUrl]],
-            //        branches:[[name: "refs/heads/${sourceBranch}"]],
-              //      extensions: []
-                //    ],
-
-            //changelog: true, poll: false);
-
-    checkout (
+    checkout(
             scm: [
-            scmGit(branches: [[name: "refs/heads/${sourceBranch}"]],
-                    extensions: [],
-                    userRemoteConfigs: [[url: gitUrl]]),
-            ],
+              $class:"GitSCM",
+                    userRemoteConfigs: [[url:gitUrl]],
+                    branches:[[name: "refs/heads/${sourceBranch}"]],
+                   extensions: []
+                    ],
+
             changelog: true, poll: false);
+
+    //checkout (
+           // scm: [
+           // scmGit(branches: [[name: "refs/heads/${sourceBranch}"]],
+               //     extensions: [],
+             //       userRemoteConfigs: [[url: gitUrl]]),
+            //],
+           // changelog: true, poll: false);
 }
  private String _generateGitUrl(String repositoryName){
     final String remoteUrlPattern = DeploymentConfigs.globalConfigs["git"]["remoteHTTPS"];
